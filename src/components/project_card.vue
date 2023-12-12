aa8227e7085aae323ef7fe14390835dc3fb4c434
Balveen Kaur <balveenkaur01@gmail.com>
2023-12-12 16:30:34 +0800

    changes to proj card

--- src/components/project_card.vue
+++ src/components/project_card.vue
@@ -2,14 +2,14 @@
 </script>
 
 <template>
-<div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-10 gap-3 justify-end w-full mx-auto items-center" style="text-align:-webkit-center">
-    <div v-for="project in project_info" :key="project.title" class="col-span-1 h-full w-full justify-center justify-self-center ">
+<div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-10 gap-3 place-items-center justify-items-center" >
+    <div v-for="project in project_info" :key="project.title" class="col-span-1 h-full place-items-center justify-items-center w-full sm:max-w-sm">
         <div class="sm:max-w-sm bg-light_color border-light_color rounded-lg shadow dark:bg-light_color dark:border-light_color h-full w-full">
             <!-- <img class="rounded-t-lg h-48" :src="project.img" alt="" id="proj_img" /> -->
             <div class="p-5">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-dark_color dark:dark_color" id="proj_title">{{ project.title }}</h5>
-                <p class="mb-3 font-normal text-dark_color" id="proj_desc">{{ project.desc }}</p>
-                <p class="mb-3 font-normal text-dark_color" id="proj_skill">{{ project.skill }}</p>
+                <p class="mb-3 font-normal text-dark_color" id="proj_desc"><span class="font-bold">Description: </span>{{ project.desc }}</p>
+                <p class="mb-3 font-normal text-dark_color" id="proj_skill"><span class="font-bold">Skills: </span>{{ project.skills }}</p>
                 <p class="mb-3 font-normal text-dark_color" id="proj_year">{{ project.year }}</p>
                 <a :href="project.link" class="inline-flex items-center px-3 py-2 text-xl text-center text-dark_color " id="proj_link">
                     <i class="fa-brands fa-github"></i>
