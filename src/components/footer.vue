7720efe9b4b1c25477a83f85c6dac03653060e4f
Balveen Kaur <balveenkaur01@gmail.com>
2023-12-18 09:21:07 +0800

    small change

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,6 +3,6 @@
 
 <template>
     <div class="container ml-5 mr-5 sm:mx-auto mb-3">
-        <p class="text-light_color text-center">Copyright (C) 2022. Balveen Kaur.</p>
+        <p class="text-light_color text-center">Copyright (C) 2023. Balveen Kaur.</p>
     </div>
 </template>
